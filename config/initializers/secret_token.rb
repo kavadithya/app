dbc19b19facf18796a1e9cb373867225344ff786
Adithya <kav.adithya@gmail.com>
2012-11-21 19:21:51 +0530

    sighnup

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RainsinstallerDemo::Application.config.secret_token = 'fa7e2ef8b2b378d95cb535bb69228f1a30d54f2b3cf5e25a4a9ffc86da6a79f7642cffc2e49a765b03b3780240a28c44baf3750f078ea15d0a3fabcb3b967b5a'
